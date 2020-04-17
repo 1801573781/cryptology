7028f5ec2280c669043bc72a38845252747e9949
1801573781 <1801573781@qq.com>
2020-04-17 20:10:30 +0800

    why

---
+++ src/aes/WKey.java
@@ -0,0 +1,15 @@
+package aes;
+
+public class WKey 
+{
+	private int wkey[] = null;
+	
+	// round const: ÂÖ³£Á¿
+	private int rc[] =
+		{
+				0x01, 0x02, 0x04, 0x08,
+				0x10, 0x20, 0x40, 0x80,
+				0x1b, 0x36				
+		};
+
+}
